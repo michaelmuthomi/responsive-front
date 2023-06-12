9effaa6321abe6533e136433848cdbddf2ef7128
Marko Anic <anicmarko9>
2023-06-12 12:24:41 +0200

    init

---
+++ front/src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./components/Home";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Navigate to="/not-found" />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = (): JSX.Element => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
