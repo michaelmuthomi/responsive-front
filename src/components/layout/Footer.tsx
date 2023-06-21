8eaabdaf87f2c68b48d6fd0f67ce3fdfb6538f0b
Marko Anic <anicmarko9>
2023-06-21 13:09:30 +0200

    deleted CSS to show an example

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,9 +6,9 @@ import { AiFillInstagram } from "react-icons/ai";
 
 const Footer = (): JSX.Element => {
   return (
-    <div className="relative flex h-full flex-col bg-customWhite px-8 pb-8 pt-8 text-base sm:px-16 sm:text-lg xl:px-40 xl:pt-16">
-      <div className="mb-4 flex flex-wrap justify-between xl:mb-24 xl:mt-4">
-        <div className="w-full space-y-8 pb-8 sm:w-1/2 xl:w-5/12">
+    <div>
+      <div>
+        <div>
           <a href="/">
             <img src="/logo.png" alt="Logo" width={140} height={65} />
           </a>
@@ -17,27 +17,25 @@ const Footer = (): JSX.Element => {
             program dedicated to helping you achieve long term weight loss
             success with the use of FDA approved medication.
           </p>
-          <div className="flex">
-            <IoLocationSharp className="contactIcon" />
-            <div className="xl:flex xl:flex-wrap">
-              <p className="font-bold xl:mr-1 xl:min-w-fit">Head Office:</p>
-              <p className="xl:min-w-fit">
-                1931 W 95th Street Chicago IL, 60643
-              </p>
+          <div>
+            <IoLocationSharp />
+            <div>
+              <p>Head Office:</p>
+              <p>1931 W 95th Street Chicago IL, 60643</p>
             </div>
           </div>
-          <div className="flex">
-            <FaPhoneAlt className="contactIcon" />
-            <div className="xl:flex xl:space-x-1">
-              <p className="font-bold">Phone:</p>
+          <div>
+            <FaPhoneAlt />
+            <div>
+              <p>Phone:</p>
               <p>773-496-4222</p>
             </div>
           </div>
         </div>
-        <div className="flex flex-wrap xl:space-x-16">
-          <div className="mr-8 py-4 xl:px-4">
-            <h5 className="mb-4 text-3xl xl:mb-8">About</h5>
-            <ul className="space-y-4">
+        <div>
+          <div>
+            <h5>About</h5>
+            <ul>
               <li>
                 <a href="/about-us">About Us</a>
               </li>
@@ -55,9 +53,9 @@ const Footer = (): JSX.Element => {
               </li>
             </ul>
           </div>
-          <div className="py-4 xl:px-4">
-            <h5 className="mb-4 text-3xl xl:mb-8">More Links</h5>
-            <ul className="space-y-4">
+          <div>
+            <h5>More Links</h5>
+            <ul>
               <li>
                 <a href="/franchise-opportunities">Franchise Opportunities</a>
               </li>
@@ -77,21 +75,19 @@ const Footer = (): JSX.Element => {
           </div>
         </div>
       </div>
-      <div className="flex flex-wrap items-center justify-between xl:space-y-0">
-        <div className="mr-4 flex space-x-6">
-          <a href="/callback-ig" className="footerIcon">
-            <AiFillInstagram className="socialMediaIcon" />
+      <div>
+        <div>
+          <a href="/callback-ig">
+            <AiFillInstagram />
           </a>
-          <a href="/callback-tw" className="footerIcon">
-            <BsTwitter className="socialMediaIcon" />
+          <a href="/callback-tw">
+            <BsTwitter />
           </a>
-          <a href="/callback-fb" className="footerIcon">
-            <FaFacebookF className="socialMediaIcon" />
+          <a href="/callback-fb">
+            <FaFacebookF />
           </a>
         </div>
-        <p className="py-4">
-          Copyright by A Better Weigh Inc. All rights reserved.
-        </p>
+        <p>Copyright by A Better Weigh Inc. All rights reserved.</p>
       </div>
     </div>
   );
